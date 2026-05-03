341044481aac87bb8f7a41810bce39c827f8c327
udenbaguse <udenbaguselord@gmail.com>
2026-05-03 10:59:27 +0700

    dynamix atribute

--- src/lib/HelloSvelte.svelte
+++ src/lib/HelloSvelte.svelte
@@ -1,5 +1,6 @@
 <script>
   const name = 'Syam';
+  const src = "https://ik.imagekit.io/udenbaguse/logo.gif?updatedAt=1769936344031"
 </script>
 
 <style>
@@ -9,3 +10,4 @@
 </style>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+<img {src} alt="logo" width="200px" />
